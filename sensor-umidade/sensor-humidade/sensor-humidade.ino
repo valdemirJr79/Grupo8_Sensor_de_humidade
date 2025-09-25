41228274481c0690b5723365022c3bfdda26fcdd
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2025-09-25 20:35:00 -0300

    atualiza código e escopo

--- sensor-umidade/sensor-humidade/sensor-humidade.ino
+++ sensor-umidade/sensor-humidade/sensor-humidade.ino
@@ -1,50 +1,95 @@
+#include <WiFi.h>
+#include <WebServer.h>
 #include "DHT.h"
 
-// Escolha o tipo de sensor
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
 #define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
-// Pino do sensor
+const char* ssid = "REDEWORK";
+const char* password = "Acessonet05";
+
+WebServer server(80);
+
+// DHT Sensor
 uint8_t DHTPin = 4; 
-
-// Inicializa o DHT
+               
+// Initialize DHT sensor.
 DHT dht(DHTPin, DHTTYPE);                
 
-float Temperature; 
+float Temperature;
 float Humidity;
-
+ 
 void setup() {
-  // Inicializa e define a taxa de comunicação serial como 115200 bits por segundos
   Serial.begin(115200);
   delay(100);
   
-  // define que o pino 4 será usado para ler dados
   pinMode(DHTPin, INPUT);
 
-  // Inicializa o dht
   dht.begin();              
-  Serial.println("Iniciando leitura do DHT...");
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+}
+void loop() {
+  
+  server.handleClient();
+  
 }
 
-void loop() {
-  // Lê temperatura
-  Temperature = dht.readTemperature(); 
-  // Lê umidade
-  Humidity = dht.readHumidity();      
+void handle_OnConnect() {
 
-  // Verifica se o valor não é número e imprime a mensagem
-  if (isnan(Temperature) || isnan(Humidity)) {
-    Serial.println("Falha ao ler do sensor DHT!");
-  } else {
-    // Imprime os dados
-    Serial.print("Temperatura: ");
-    Serial.print(Temperature);
-    Serial.println(" °C");
+ Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity 
+  server.send(200, "text/html", SendHTML(Temperature,Humidity)); 
+}
 
-    Serial.print("Umidade: ");
-    Serial.print(Humidity);
-    Serial.println(" %");
-  }
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
 
-  // Aguarda 2 segundos entre leituras
-  delay(2000); 
+String SendHTML(float Temperaturestat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>ESP32 Weather Report</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP32 Weather Report</h1>\n";
+  
+  ptr +="<p>Temperature: ";
+  ptr +=(int)Temperaturestat;
+  ptr +="°C</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=(int)Humiditystat;
+  ptr +="%</p>";
+  
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
 }
